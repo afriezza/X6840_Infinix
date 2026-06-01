86f4a2465dc1be157d27ff1d75c303269be5fd0c
afriezza <wongjen07@gmail.com>
2026-06-01 23:56:06 +0700

    Change product inheritance in twrp_X6840.mk
    Updated product inheritance for build configuration.

--- twrp_X6840.mk
+++ twrp_X6840.mk
@@ -1,4 +1,7 @@
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64bit.mk)
+# Ganti inherit yang core_64bit dengan ini
+$(call inherit-product, build/make/target/product/full_base.mk)
+
+# Inherit yang ini tetep ada
 $(call inherit-product, $(SRC_TARGET_DIR)/product/minimal_header.mk)
 
 PRODUCT_DEVICE := X6840
@@ -6,3 +9,6 @@ PRODUCT_NAME := twrp_X6840
 PRODUCT_BRAND := Infinix
 PRODUCT_MODEL := Infinix Smart 20
 PRODUCT_MANUFACTURER := infinix
+
+# Tambahin ini biar gak error pas lunch
+PRODUCT_RESTRICT_VENDOR_FILES := false
