9bcde2f935e6e58a8cc2502aab4db0a20755ed77
afriezza <wongjen07@gmail.com>
2026-06-01 16:46:27 +0700

    Create twrp_X6840.mk

---
+++ twrp_X6840.mk
@@ -0,0 +1,8 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/minimal_header.mk)
+
+PRODUCT_DEVICE := X6840
+PRODUCT_NAME := twrp_X6840
+PRODUCT_BRAND := Infinix
+PRODUCT_MODEL := Infinix Smart 20
+PRODUCT_MANUFACTURER := infinix
