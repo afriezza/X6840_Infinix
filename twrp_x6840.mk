6d6f67c3fa9623679acf9867f43398f6985fabbe
afriezza <wongjen07@gmail.com>
2026-06-02 00:25:56 +0700

    Update twrp_x6840.mk

--- twrp_x6840.mk
+++ twrp_x6840.mk
@@ -1,13 +1,13 @@
-# Jangan panggil inherit-product yang aneh-aneh dulu
-# Kita cuma butuh definisikan variabel dasar biar build sistem kenal device lo
-
-PRODUCT_DEVICE := X6840
-PRODUCT_NAME := twrp_X6840
+PRODUCT_DEVICE := x6840
+PRODUCT_NAME := twrp_x6840
 PRODUCT_BRAND := Infinix
 PRODUCT_MODEL := Infinix Smart 20
 PRODUCT_MANUFACTURER := infinix
 
-# Biar sistem tahu ini recovery
 PRODUCT_PACKAGES += \
     recovery \
     twrp
+
+PRODUCT_RESTRICT_VENDOR_FILES := false
+$(call inherit-product, vendor/omni/config/common.mk)
+
