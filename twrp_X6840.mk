df5d928a3ef557529275806e0a5ef8eeff153cf3
afriezza <wongjen07@gmail.com>
2026-06-02 00:09:39 +0700

    Update twrp_X6840.mk

--- twrp_X6840.mk
+++ twrp_X6840.mk
@@ -1,8 +1,5 @@
-# Ganti inherit yang core_64bit dengan ini
-$(call inherit-product, build/make/target/product/full_base.mk)
-
-# Inherit yang ini tetep ada
-$(call inherit-product, $(SRC_TARGET_DIR)/product/minimal_header.mk)
+# Jangan panggil inherit-product yang aneh-aneh dulu
+# Kita cuma butuh definisikan variabel dasar biar build sistem kenal device lo
 
 PRODUCT_DEVICE := X6840
 PRODUCT_NAME := twrp_X6840
@@ -10,5 +7,7 @@ PRODUCT_BRAND := Infinix
 PRODUCT_MODEL := Infinix Smart 20
 PRODUCT_MANUFACTURER := infinix
 
-# Tambahin ini biar gak error pas lunch
-PRODUCT_RESTRICT_VENDOR_FILES := false
+# Biar sistem tahu ini recovery
+PRODUCT_PACKAGES += \
+    recovery \
+    twrp
