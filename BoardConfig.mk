7bd23fee0f032da604d68a0ec0272763a1051846
afriezza <wongjen07@gmail.com>
2026-06-01 16:40:47 +0700

    Add files via upload

---
+++ BoardConfig.mk
@@ -0,0 +1,30 @@
+DEVICE_PATH := device/infinix/mt6768
+
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 := 
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+
+# APEX
+DEXPREOPT_GENERATE_APEX_IMAGE := true
+
+# Kernel
+TARGET_NO_KERNEL := false
+BOARD_USES_RECOVERY_AS_BOOT := false
+
+# Partitions (Virtual A/B & Dynamic Partition)
+BOARD_FLASH_BLOCK_SIZE := 131072
+BOARD_VENDOR_BOOTIMAGE_PARTITION_SIZE := 67108864
+BOARD_MOVE_RECOVERY_RESOURCES_TO_VENDOR_BOOT := true
+
+# OrangeFox Flags
+FOX_VERSION := 11.1
+OF_MAINTAINER := "Opreker Infinix"
+OF_DEVICE_WITHOUT_PERSIST := 1
+OF_QUICK_BACKUP_LIST := "Boot;Vendor_Boot;Data;"
+MAIN_BLOCK := /dev/block/mmcblk0
