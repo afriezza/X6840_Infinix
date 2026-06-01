8e98c86fbb629ed4de7bad0ac7ab406e011c9bf9
afriezza <wongjen07@gmail.com>
2026-06-02 00:39:59 +0700

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -2,6 +2,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/twrp_X6840.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_X6840-eng \
-    twrp_X6840-user \
-    twrp_X6840-userdebug
+    twrp_x6840-eng \
+    twrp_x6840-user \
+    twrp_x6840-userdebug
